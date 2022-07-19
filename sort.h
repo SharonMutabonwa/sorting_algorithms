203c43d79821775556bd41aabcd9e5463e274e2b
SharonMutabonwa <mutaron37@gmail.com>
2022-07-19 21:45:29 +0000

    A function that sorts an array of integers in ascending order using the Quick sort algorithm

--- sort.h
+++ sort.h
@@ -26,6 +26,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
+size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size);
+void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size);
 
 #endif
